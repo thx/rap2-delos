cd55de448826bff45db2a83003df3e59a974dadb
zhcsyncer <zhcsyncer@gmail.com>
2018-02-06 19:01:45 +0800

    fix bug: #72, 自定义 sql 查询中使用`Model.getTableName`获取表名

--- src/routes/analytics.ts
+++ src/routes/analytics.ts
@@ -1,4 +1,7 @@
 import router from './router'
+import Repository from "../models/bo/repository";
+import Logger from "../models/bo/logger";
+import User from "../models/bo/user";
 const moment = require('moment')
 const Sequelize = require('sequelize')
 const SELECT = { type: Sequelize.QueryTypes.SELECT }
@@ -14,7 +17,7 @@ router.get('/app/analytics/repositories/created', async (ctx) => {
         DATE(createdAt) AS label,
         COUNT(*) as value
     FROM
-        RAP2_DELOS_APP.repositories
+        ${Repository.getTableName()}
     WHERE
         createdAt >= '${start}' AND createdAt <= '${end}'
     GROUP BY label
@@ -39,7 +42,7 @@ router.get('/app/analytics/repositories/updated', async (ctx) => {
         DATE(updatedAt) AS label,
         COUNT(*) as value
     FROM
-        RAP2_DELOS_APP.repositories
+        ${Repository.getTableName()}
     WHERE
         updatedAt >= '${start}' AND updatedAt <= '${end}'
     GROUP BY label
@@ -65,9 +68,9 @@ router.get('/app/analytics/users/activation', async (ctx) => {
         users.fullname AS fullname,
         COUNT(*) AS value
     FROM
-        loggers
+        ${Logger.getTableName()} loggers
             LEFT JOIN
-        (users) ON (loggers.userId = users.id)
+        ${User.getTableName()} users ON (loggers.userId = users.id)
     WHERE
         loggers.updatedAt >= '${start}' AND loggers.updatedat <= '${end}'
     GROUP BY loggers.userId
@@ -90,9 +93,10 @@ router.get('/app/analytics/repositories/activation', async (ctx) => {
         repositories.name,
         COUNT(*) AS value
     FROM
-        loggers
-            LEFT JOIN
-        (repositories) ON (loggers.repositoryId = repositories.id)
+        ${Logger.getTableName()} loggers
+    LEFT JOIN
+        ${Repository.getTableName()} repositories
+        ON (loggers.repositoryId = repositories.id)
     WHERE
         loggers.repositoryId IS NOT NULL
             AND loggers.updatedAt >= '${start}'
