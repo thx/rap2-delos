48eacfebfce2b15df715317827e0515b2f589895
Bosn <bosn@outlook.com>
2018-04-20 13:23:21 +0800

    fix access bug

--- src/service/repository.ts
+++ src/service/repository.ts
@@ -1,10 +1,18 @@
-import { Repository } from "../models"
+import { Repository, RepositoriesMembers } from "../models"
 import OrganizationService from "./organization";
 
 export default class RepositoryService {
   public static async canUserAccessRepository(userId: number, repositoryId: number): Promise<boolean> {
     const repo = await Repository.findById(repositoryId)
+    if (!repo) return false
     if (repo.creatorId === userId || repo.ownerId === userId) return true
+    const memberExistsNum = await RepositoriesMembers.count({
+      where: {
+        userId,
+        repositoryId,
+      }
+    })
+    if (memberExistsNum > 0) return true
     return OrganizationService.canUserAccessOrganization(userId, repo.organizationId)
   }
 }
