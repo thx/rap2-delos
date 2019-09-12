5882daef32625482dac9bd12573997f98dc07059
bigfengyu <kuailewudi@yeah.net>
2019-09-12 11:12:21 +0800

    fix: isneedlogin

--- src/routes/base.ts
+++ src/routes/base.ts
@@ -4,7 +4,7 @@ const inTestMode = process.env.TEST_MODE === 'true'
 
 
 export async function isLoggedIn(ctx: ParameterizedContext<any, any>, next: () => Promise<any>) {
-  if (!inTestMode && (!ctx.session || !ctx.session.id)) {
+  if (!inTestMode && (!ctx.session || ctx.session.id == undefined)) {
     ctx.body = {
       isOk: false,
       errMsg: 'need login',
