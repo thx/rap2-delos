6543387835c753cef7076011036e2435879bfef3
bigfengyu <kuailewudi@yeah.net>
2019-11-28 14:51:30 +0800

    fix: initSchema & updateSchema

--- src/scripts/initSchema.ts
+++ src/scripts/initSchema.ts
@@ -2,14 +2,14 @@ import sequelize from "../models/sequelize"
 
 sequelize
   .sync({
-    alter: true
+    force: true,
   })
   .then(() => {
-    console.log("成功升级 DB Schema")
+    console.log("成功初始化 DB Schema")
     process.exit(0)
   })
   .catch(e => {
-    console.log("升级 DB Schema 中遇到了错误")
+    console.log("初始化 DB Schema 中遇到了错误")
     console.log(e)
     process.exit(0)
   })
