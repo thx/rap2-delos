886b16f037735981cd73a91828e1c0ddc5b354cf
bigfengyu <kuailewudi@yeah.net>
2019-12-30 10:35:34 +0800

    fix: 分页处理

--- src/scripts/setToken.ts
+++ src/scripts/setToken.ts
@@ -4,20 +4,41 @@ import sequelize from "../models/sequelize";
 import { Repository } from "../models";
 const chalk = require("chalk");
 
-sequelize.model(Repository);
-Repository.findAll({
-  where: {
-    token: null
+(async () => {
+  sequelize.model(Repository);
+
+  const cnt = await Repository.count({
+    where: {
+      token: null
+    }
+  });
+
+
+  const limit = 500;
+
+  console.log(`共有${cnt}条数据`);
+
+
+  for (let offset = 0; offset < cnt; offset += limit) {
+    console.log(`正在处理offset ${offset}, limit ${limit}`);
+    const rows = await Repository.findAll({
+      where: {
+        token: null
+      },
+      limit,
+      offset
+    });
+    await Promise.all(
+      rows.map(async repo => {
+        if (!repo.token) {
+          repo.token = nanoid(32);
+          await repo.save();
+          console.log(
+            chalk.green(repo.name + "添加了默认token：" + repo.token)
+          );
+        }
+      })
+    );
   }
-}).then(async rows => {
-  await Promise.all(
-    rows.map(async repo => {
-      if (!repo.token) {
-        repo.token = nanoid(32);
-        await repo.save();
-        console.log(chalk.green(repo.name + "添加了默认token：" + repo.token));
-      }
-    })
-  );
-  process.exit(0);
-});
+  process.exit(0)
+})();
